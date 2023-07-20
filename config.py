74a36575c2bca4db3da57878776fb9114a879cd8
Shadow229 <55843220+Shadow229@users.noreply.github.com>
2023-07-20 13:11:43 +0100

    Update config.py
    variable name change

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 #plex config
 plex_api_url = 'http://192.168.111.101:32400'  #change to your Plex API URL
 plex_api_request = '/library/sections/1/all' #the api request 
-plex_token = '?X-Plex-Token=<YOUR PLEX TOKEN>' #change to your token for auth
+plex_token = '<YOUR PLEX TOKEN>' #change to your token for auth
 
 #radarr config
 radarr_url = 'http://192.168.111.101:7878/api/v3' #change to your Radarr API URL
@@ -10,12 +10,12 @@ radarr_api_key = '<YOUR RADARR API KEY>' #change to your Radarr API key
 
 #overseerr config
 overseerr_url = 'http://192.168.111.101:5055/api/v1'  #change to your Overseerr URL
-overseerr_api_key = 'YOUR OVERSEERR API KEY='  #change to your Overseerr API key
+overseerr_api_key = '<YOUR OVERSEERR API KEY>'  #change to your Overseerr API key
 overseerr_root_folder = '/media' #change to your radarr root folder
 
 #The Movie Database
 tmdb_url = 'https://api.themoviedb.org/3/'
-tmdb_api_key = 'YOUR TMDB API KEY'  #change to your TMDB API key
+tmdb_api_key = '<YOUR TMDB API KEY>'  #change to your TMDB API key
 
 #File location
-scriptLocation = '/mnt/user/Scripts/' #set to the share the scripts are saved on. Set to '' if testing from VSCode
+script_dir = '/mnt/user/Scripts/' #set to the share the scripts are saved on. Set to '' if testing from VSCode
